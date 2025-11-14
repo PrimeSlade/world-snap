1964342020d02491bf7b5530e0f59a1b1f7ca6e5
Slade <saib6064@gmail.com>
2025-11-15 00:53:48 +0700

    feat(auth): create entities for auth module
    Creates RegisterAuth and SignInAuth classes with properties
    for name, email, and password, and decorates them with
    ApiProperty for Swagger documentation.

--- backend/src/auth/entities/auth.entity.ts
+++ backend/src/auth/entities/auth.entity.ts
@@ -1,1 +1,20 @@
-export class Auth {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class RegisterAuth {
+  @ApiProperty({ example: 'Okok' })
+  name: string;
+
+  @ApiProperty({ example: 'okok@gmail.com' })
+  email: string;
+
+  @ApiProperty({ example: '11111' })
+  password: string;
+}
+
+export class SignInAuth {
+  @ApiProperty({ example: 'okok@gmail.com' })
+  email: string;
+
+  @ApiProperty({ example: '11111' })
+  password: string;
+}
